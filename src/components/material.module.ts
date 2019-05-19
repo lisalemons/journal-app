820145004ff85474f65d36272bf703bfc2e6e932
Lisa Lemons <lisalemons@Lisas-MacBook-Pro.local>
2019-05-19 15:20:43 -0400

    basic styles and angular material set up

---
+++ src/components/material.module.ts
@@ -0,0 +1,15 @@
+
+import { NgModule } from '@angular/core';
+import {
+  MatTabsModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatTabsModule,
+  ],
+  exports: [
+    MatTabsModule,
+  ],
+})
+export class MaterialModule { }
